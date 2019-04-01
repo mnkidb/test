9d6ae70bbb08e46b23b26848029c50fca456fd46
mnkidb <mnkidb@gmail.com>
2019-04-01 14:18:02 +0900

    04011417

--- WebProj/src/com/internousdev/webproj/action/InquiryAction.java
+++ WebProj/src/com/internousdev/webproj/action/InquiryAction.java
@@ -2,9 +2,10 @@ package com.internousdev.webproj.action;
 
 import com.opensymphony.xwork2.ActionSupport;
 
-public class HelloStrutsAction extends ActionSupport{
+
+public class InquiryAction extends ActionSupport{
+
 	public String execute(){
 		return SUCCESS;
 	}
-
 }
