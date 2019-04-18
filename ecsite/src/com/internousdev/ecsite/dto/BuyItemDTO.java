437ab523d6c1d25ce6ff2dc572b292ef42dc4eb8
mnkidb <mnkidb@gmail.com>
2019-04-18 13:15:57 +0900

    0418

--- ecsite/src/com/internousdev/ecsite/dto/BuyItemDTO.java
+++ ecsite/src/com/internousdev/ecsite/dto/BuyItemDTO.java
@@ -2,9 +2,9 @@ package com.internousdev.ecsite.dto;
 
 public class BuyItemDTO {
 
-	public int id;
-	public String itemName;
-	public String itemPrice;
+	private int id;
+	private String itemName;
+	private String itemPrice;
 
 	public String getItemName(){
 		return itemName;
